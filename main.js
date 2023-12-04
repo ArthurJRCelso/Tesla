73cf7114bc0c402e20632aeb78db4d29b1a63bb2
arthur <arthur.rodrigues1784@gmail.com>
2023-12-04 17:06:02 +0000

    scroll

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const menuBtn = document.querySelector('#menuBtn')
-
-
+const aside = document.querySelector('aside')
+const closeBtn = document.querySelector('.close')
+let width = window.innerWidth;
+let height = window.innerHeight;
+const sections = document.querySelectorAll('section')
+const Section = document.querySelector('section')
 
 
 menuBtn.addEventListener('click', openMenu)
+closeBtn.addEventListener('click', closeMenu)
+window.addEventListener('resize', closeAuto)
 
-
-
-
-function openMenu() {
-    
+function openMenu(e) {
+    e.preventDefault()
+    aside.classList.toggle('visibility')
 }
+
+function closeMenu() {
+    aside.classList.toggle('visibility')
+}
+
+function closeAuto() {
+    if (window.innerWidth > 600) {
+      closeMenu();
+    }
+}
+
+ 
